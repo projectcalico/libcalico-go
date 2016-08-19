b9b71d26648a3f5c8053356a779ede012c49acbd
Rob Brockbank <rob@tigera.io>
2016-08-19 15:11:17 -0700

    simplify scope type

--- lib/scope/scope.go
+++ lib/scope/scope.go
@@ -14,71 +14,10 @@
 
 package scope
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
-type Scope int
+type Scope string
 
 const (
-	Undefined = iota
-	Global
-	Node
+	Undefined Scope = ""
+	Global          = "global"
+	Node            = "node"
 )
-
-const (
-	UndefinedStr = "undefined"
-	GlobalStr    = "global"
-	NodeStr      = "node"
-)
-
-// UnmarshalJSON implements the json.Unmarshaller interface.
-func (f *Scope) UnmarshalJSON(b []byte) error {
-	var value string
-	err := json.Unmarshal(b, &value)
-	if err != nil {
-		return err
-	}
-
-	switch value {
-	case UndefinedStr:
-		*f = Undefined
-	case GlobalStr:
-		*f = Global
-	case NodeStr:
-		*f = Node
-	default:
-		return fmt.Errorf("Unrecognised scope value '%s'", value)
-	}
-
-	return nil
-}
-
-// String returns a friendly string value for this scope.
-func (f *Scope) String() string {
-	switch *f {
-	case Undefined:
-		return "scope:undefined"
-	case Global:
-		return "scope:global"
-	case Node:
-		return "scope:node"
-	default:
-		return "scope:unknown"
-	}
-}
-
-// MarshalJSON implements the json.Marshaller interface.
-func (f Scope) MarshalJSON() ([]byte, error) {
-	switch f {
-	case Undefined:
-		return json.Marshal(UndefinedStr)
-	case Global:
-		return json.Marshal(GlobalStr)
-	case Node:
-		return json.Marshal(NodeStr)
-	default:
-		return nil, fmt.Errorf("unknown scope value `%d`", f)
-	}
-}
