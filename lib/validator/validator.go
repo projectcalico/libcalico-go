436241f6b255dd4fb57d20b0ec1405caa10442b6
Shaun Crampton <shaun@tigera.io>
2016-09-21 14:50:59 +0100

    Allow log and empty string for rule actions.

--- lib/validator/validator.go
+++ lib/validator/validator.go
@@ -34,7 +34,7 @@ var (
 	nameRegex          = regexp.MustCompile("^[a-zA-Z0-9_.-]+$")
 	labelRegex         = regexp.MustCompile("^[a-zA-Z_./-][a-zA-Z0-9_./-]*$")
 	actionRegex        = regexp.MustCompile("^(nextTier|allow|deny)$")
-	backendActionRegex = regexp.MustCompile("^(next-tier|allow|deny)$")
+	backendActionRegex = regexp.MustCompile("^(next-tier|allow|deny|log|)$")
 	protocolRegex      = regexp.MustCompile("^(tcp|udp|icmp|icmpv6|sctp|udplite)$")
 )
 
