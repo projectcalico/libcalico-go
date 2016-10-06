0ddf9de2807feb4ef2953d78fab33e0bc78d2fdb
Shaun Crampton <shaun@tigera.io>
2016-10-06 11:34:02 +0100

    Fix imports/builds to use new projectcalico/libcalico-go repo location.

--- lib/client/ipam.go
+++ lib/client/ipam.go
@@ -20,10 +20,10 @@ import (
 	"os"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/tigera/libcalico-go/lib/api"
-	"github.com/tigera/libcalico-go/lib/backend/model"
-	"github.com/tigera/libcalico-go/lib/errors"
-	"github.com/tigera/libcalico-go/lib/net"
+	"github.com/projectcalico/libcalico-go/lib/api"
+	"github.com/projectcalico/libcalico-go/lib/backend/model"
+	"github.com/projectcalico/libcalico-go/lib/errors"
+	"github.com/projectcalico/libcalico-go/lib/net"
 )
 
 const (
