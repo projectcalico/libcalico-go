604b8dffd609ae016fb5dc496881d9c443400a25
gunjan5 <patelgunjan5@gmail.com>
2016-10-19 17:04:26 -0700

    HostEndpoint e2e tests

---
+++ lib/client/hostendpoint_e2e_test.go
@@ -0,0 +1,303 @@
+// Copyright (c) 2016 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Test cases (HostEndpoint object e2e):
+// Test 1: Pass two fully populated HostEndpointSpecs and expect the series of operations to succeed.
+// Test 2: Pass one partially populated HostEndpointSpec and another fully populated HostEndpointSpec and expect the series of operations to succeed.
+// Test 3: Pass one fully populated HostEndpointSpec and another empty HostEndpointSpec and expect the series of operations to succeed.
+// Test 4: Pass two fully populated HostEndpointSpecs with two HostEndpointMetadata (one IPv4 and another IPv6) and expect the series of operations to succeed.
+
+// Series of operations each test goes through:
+// Update meta1 - check for failure (because it doesn't exist).
+// Create meta1 with spec1.
+// Apply meta2 with spec2.
+// Get meta1 and meta2, compare spec1 and spec2.
+// Update meta1 with spec2.
+// Get meta1 compare spec2.
+// List (empty Meta) ... Get meta1 and meta2.
+// List (using Meta1) ... Get meta1.
+// Delete meta1.
+// Get meta1 ... fail.
+// Delete meta2.
+// List (empty Meta) ... Get no entries (should not error).
+
+package client_test
+
+import (
+	"errors"
+	"log"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	"github.com/projectcalico/libcalico-go/lib/client"
+
+	"github.com/projectcalico/libcalico-go/lib/api"
+	"github.com/projectcalico/libcalico-go/lib/testutils"
+
+	cnet "github.com/projectcalico/libcalico-go/lib/net"
+)
+
+var _ = Describe("HostEndpoint tests", func() {
+
+	DescribeTable("HostEndpoint e2e tests",
+		func(meta1, meta2 api.HostEndpointMetadata, spec1, spec2 api.HostEndpointSpec) {
+
+			// Erase etcd clean.
+			testutils.CleanEtcd()
+
+			// Create a new client.
+			c, err := testutils.NewClient("")
+			if err != nil {
+				log.Println("Error creating client:", err)
+			}
+			By("Updating the HostEndpoint before it is created")
+			_, outError := hostEndpointUpdate(c, &api.HostEndpoint{Metadata: meta1, Spec: spec1})
+
+			// Should return an error.
+			Expect(outError.Error()).To(Equal(errors.New("resource does not exist: HostEndpoint(hostname=hostname1, name=host1)").Error()))
+
+			By("Create, Apply, Get and compare")
+
+			// Create a HostEndpoint with meta1 and spec1.
+			_, outError = hostEndpointCreate(c, &api.HostEndpoint{Metadata: meta1, Spec: spec1})
+
+			// Apply a HostEndpoint with meta2 and spec2.
+			_, outError = hostEndpointApply(c, &api.HostEndpoint{Metadata: meta2, Spec: spec2})
+
+			// Get HostEndpoint with meta1.
+			outHostEndpoint1, outError1 := c.HostEndpoints().Get(meta1)
+			log.Println("Out HostEndpoint object: ", outHostEndpoint1)
+
+			// Get HostEndpoint with meta2.
+			outHostEndpoint2, outError2 := c.HostEndpoints().Get(meta2)
+			log.Println("Out HostEndpoint object: ", outHostEndpoint2)
+
+			// Should match spec1 & outHostEndpoint1 and outHostEndpoint2 & spec2 and errors to be nil.
+			Expect(outHostEndpoint1.Spec).To(Equal(spec1))
+			Expect(outHostEndpoint2.Spec).To(Equal(spec2))
+			Expect(outError1).To(BeNil())
+			Expect(outError2).To(BeNil())
+
+			By("Update, Get and compare")
+
+			// Update meta1 HostEndpoint with spec2.
+			hostEndpointUpdate(c, &api.HostEndpoint{Metadata: meta1, Spec: spec2})
+
+			// Get HostEndpoint with meta1.
+			outHostEndpoint1, outError1 = c.HostEndpoints().Get(meta1)
+
+			// Assert the Spec for HostEndpoint with meta1 matches spec2 and no error.
+			Expect(outHostEndpoint1.Spec).To(Equal(spec2))
+			Expect(outError1).To(BeNil())
+
+			By("List all the HostEndpoints and compare")
+
+			// Get a list of HostEndpoints.
+			hostEndpointList, outError := c.HostEndpoints().List(api.HostEndpointMetadata{})
+			log.Println("Get HostEndpoint list returns: ", hostEndpointList.Items)
+			metas := []api.HostEndpointMetadata{meta1, meta2}
+			expectedHostEndpoints := []api.HostEndpoint{}
+			// Go through meta list and append them to expectedHostEndpoints.
+			for _, v := range metas {
+				p, _ := c.HostEndpoints().Get(v)
+				expectedHostEndpoints = append(expectedHostEndpoints, *p)
+			}
+
+			// Assert the returned hostEndpointList is has the meta1 and meta2 HostEndpoints.
+			Expect(hostEndpointList.Items).To(Equal(expectedHostEndpoints))
+
+			By("List a specific HostEndpoint and compare")
+
+			// Get a HostEndpoint list with meta1.
+			hostEndpointList, outError = c.HostEndpoints().List(meta1)
+			log.Println("Get HostEndpoint list returns: ", hostEndpointList.Items)
+
+			// Get a HostEndpoint with meta1.
+			outHostEndpoint1, outError1 = c.HostEndpoints().Get(meta1)
+
+			// Assert they are equal and no errors.
+			Expect(hostEndpointList.Items[0].Spec).To(Equal(outHostEndpoint1.Spec))
+			Expect(outError1).To(BeNil())
+
+			By("Delete, Get and assert error")
+
+			// Delete a HostEndpoint with meta1.
+			outError1 = c.HostEndpoints().Delete(meta1)
+
+			// Get a HostEndpoint with meta1.
+			_, outError = c.HostEndpoints().Get(meta1)
+
+			// Expect an error since the HostEndpoint was deleted.
+			Expect(outError.Error()).To(Equal(errors.New("resource does not exist: HostEndpoint(hostname=hostname1, name=host1)").Error()))
+
+			// Delete the second HostEndpoint with meta2.
+			outError1 = c.HostEndpoints().Delete(meta2)
+
+			By("Delete all the HostEndpoints, Get HostEndpoint list and expect empty HostEndpoint list")
+
+			// Both HostEndpoints are deleted in the calls above.
+			// Get the list of all the HostEndpoints.
+			hostEndpointList, outError = c.HostEndpoints().List(api.HostEndpointMetadata{})
+			log.Println("Get HostEndpoint list returns: ", hostEndpointList.Items)
+
+			// Create an empty HostEndpoint list.
+			// Note: you can't use make([]api.HostEndpoint, 0) because it creates an empty underlying struct,
+			// whereas new([]api.HostEndpoint) just returns a pointer without creating an empty struct.
+			emptyhostEndpointList := new([]api.HostEndpoint)
+
+			// Expect returned hostEndpointList to contain empty hostEndpointList.
+			Expect(hostEndpointList.Items).To(Equal(*emptyhostEndpointList))
+
+		},
+
+		// Test 1: Pass two fully populated HostEndpointSpecs and expect the series of operations to succeed.
+		Entry("Two fully populated HostEndpointSpecs",
+			api.HostEndpointMetadata{
+				Name: "host1",
+				Node: "hostname1",
+				Labels: map[string]string{
+					"app":  "app-abc",
+					"prod": "no",
+				}},
+			api.HostEndpointMetadata{
+				Name: "host2",
+				Node: "hostname2",
+				Labels: map[string]string{
+					"app":  "app-xyz",
+					"prod": "yes",
+				}},
+			api.HostEndpointSpec{
+				InterfaceName: "eth0",
+				ExpectedIPs:   []cnet.IP{testutils.MustParseIP("10.0.0.0"), testutils.MustParseIP("20.0.0.0")},
+				Profiles:      []string{"profile1", "profile2"},
+			},
+			api.HostEndpointSpec{
+				InterfaceName: "eth1",
+				ExpectedIPs:   []cnet.IP{testutils.MustParseIP("192.168.0.0"), testutils.MustParseIP("192.168.1.1")},
+				Profiles:      []string{"profile3", "profile4"},
+			}),
+
+		// Test 2: Pass one partially populated HostEndpointSpec and another fully populated HostEndpointSpec and expect the series of operations to succeed.
+		Entry("One partially populated HostEndpointSpec and another fully populated HostEndpointSpec",
+			api.HostEndpointMetadata{
+				Name: "host1",
+				Node: "hostname1",
+				Labels: map[string]string{
+					"app":  "app-abc",
+					"prod": "no",
+				}},
+			api.HostEndpointMetadata{
+				Name: "host2",
+				Node: "hostname2",
+				Labels: map[string]string{
+					"app":  "app-xyz",
+					"prod": "yes",
+				}},
+			api.HostEndpointSpec{
+				InterfaceName: "eth0",
+			},
+			api.HostEndpointSpec{
+				InterfaceName: "eth1",
+				ExpectedIPs:   []cnet.IP{testutils.MustParseIP("192.168.0.0"), testutils.MustParseIP("192.168.1.1")},
+				Profiles:      []string{"profile3", "profile4"},
+			}),
+
+		// Test 3: Pass one fully populated HostEndpointSpec and another empty HostEndpointSpec and expect the series of operations to succeed.
+		Entry("One fully populated HostEndpointSpec and another empty HostEndpointSpec",
+			api.HostEndpointMetadata{
+				Name: "host1",
+				Node: "hostname1",
+				Labels: map[string]string{
+					"app":  "app-abc",
+					"prod": "no",
+				}},
+			api.HostEndpointMetadata{
+				Name: "host2",
+				Node: "hostname2",
+				Labels: map[string]string{
+					"app":  "app-xyz",
+					"prod": "yes",
+				}},
+			api.HostEndpointSpec{
+				InterfaceName: "eth0",
+				ExpectedIPs:   []cnet.IP{testutils.MustParseIP("10.0.0.0"), testutils.MustParseIP("20.0.0.0")},
+				Profiles:      []string{"profile1", "profile2"},
+			},
+			api.HostEndpointSpec{}),
+
+		// Test 4: Pass two fully populated HostEndpointSpecs with two HostEndpointMetadata (one IPv4 and another IPv6) and expect the series of operations to succeed.
+		Entry("Two fully populated HostEndpointSpecs with two HostEndpointMetadata (one IPv4 and another IPv6)",
+			api.HostEndpointMetadata{
+				Name: "host1",
+				Node: "hostname1",
+				Labels: map[string]string{
+					"app":  "app-abc",
+					"prod": "no",
+				}},
+			api.HostEndpointMetadata{
+				Name: "host2",
+				Node: "hostname2",
+				Labels: map[string]string{
+					"app":  "app-xyz",
+					"prod": "yes",
+				}},
+			api.HostEndpointSpec{
+				InterfaceName: "eth0",
+				ExpectedIPs:   []cnet.IP{testutils.MustParseIP("10.0.0.0"), testutils.MustParseIP("192.168.1.1")},
+				Profiles:      []string{"profile1", "profile2"},
+			},
+			api.HostEndpointSpec{
+				InterfaceName: "eth1",
+				ExpectedIPs:   []cnet.IP{testutils.MustParseIP("fe80::00"), testutils.MustParseIP("fe80::33")},
+				Profiles:      []string{"profile3", "profile4"},
+			}),
+	)
+
+})
+
+// hostEndpointCreate takes client and api.HostEndpoint and creates a HostEndpoint.
+func hostEndpointCreate(c *client.Client, p *api.HostEndpoint) (*api.HostEndpoint, error) {
+
+	pOut, errOut := c.HostEndpoints().Create(p)
+
+	if errOut != nil {
+		log.Printf("Error creating HostEndpoint: %s\n", errOut)
+	}
+	return pOut, errOut
+
+}
+
+// hostEndpointApply takes client and api.HostEndpoint and applies the HostEndpoint config to
+// an existing HostEndpoint or creates a new one if it doesn't already exist.
+func hostEndpointApply(c *client.Client, p *api.HostEndpoint) (*api.HostEndpoint, error) {
+
+	pOut, errOut := c.HostEndpoints().Apply(p)
+	if errOut != nil {
+		log.Printf("Error applying HostEndpoint: %s\n", errOut)
+	}
+
+	return pOut, errOut
+}
+
+// hostEndpointUpdate takes client and api.HostEndpoint and updates an existing HostEndpoint.
+func hostEndpointUpdate(c *client.Client, p *api.HostEndpoint) (*api.HostEndpoint, error) {
+
+	pOut, errOut := c.HostEndpoints().Update(p)
+	if errOut != nil {
+		log.Printf("Error updating HostEndpoint: %s\n", errOut)
+	}
+
+	return pOut, errOut
+}
