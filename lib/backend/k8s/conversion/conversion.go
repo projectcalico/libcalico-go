70d1b867a4bef5c59c7b55c56d20444ba19db65b
Saurabh Mohan <saurabh@tigera.io>
2017-11-29 12:07:32 -0800

    Fix rebase issue.

--- lib/backend/k8s/conversion/conversion.go
+++ lib/backend/k8s/conversion/conversion.go
@@ -25,7 +25,6 @@ import (
 	kapiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/projectcalico/libcalico-go/lib/apiconfig"
 	apiv3 "github.com/projectcalico/libcalico-go/lib/apis/v3"
 	"github.com/projectcalico/libcalico-go/lib/backend/model"
 	"github.com/projectcalico/libcalico-go/lib/names"
