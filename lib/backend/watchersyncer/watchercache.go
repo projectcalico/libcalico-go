476739a7a882d2a79effd8b96fd5de34d1d072fe
Casey Davenport <davenport.cas@gmail.com>
2021-08-06 10:25:48 -0700

    Use ResourceVersionMatch and watch bookmarks

--- lib/backend/watchersyncer/watchercache.go
+++ lib/backend/watchersyncer/watchercache.go
@@ -180,8 +180,9 @@ func (wc *watcherCache) resyncAndCreateWatcher(ctx context.Context) {
 				wc.resourceType.UpdateProcessor.OnSyncerStarting()
 			}
 
-			// Start the sync by Listing the current resources.
-			l, err := wc.client.List(ctx, wc.resourceType.ListInterface, "0")
+			// Start the sync by Listing the current resources. Start from the current watch revision, which will
+			// be 0 at start of day or the latest received revision.
+			l, err := wc.client.List(ctx, wc.resourceType.ListInterface, wc.currentWatchRevision)
 			if err != nil {
 				// Failed to perform the list.  Pause briefly (so we don't tight loop) and retry.
 				wc.logger.WithError(err).Info("Failed to perform list of current data during resync")
